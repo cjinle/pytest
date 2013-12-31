29a127dc3ab2baff2033f02f44c0289c3a47dc28
Jinle Chen <cjinle@gmail.com>
2013-12-31 17:19:10 +0800

    from git_push.sh

--- ssh.py
+++ ssh.py
@@ -4,8 +4,9 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.1.55', 22, 'root', '123456')
+ssh.connect('192.168.56.1', 22, 'root', '123456')
 stdin, stdout, stderr = ssh.exec_command("df")
+print stdout.readlines();
 for i in stdout.readlines():
     print i
 ssh.close()
