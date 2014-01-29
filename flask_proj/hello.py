b045b3a21a11699c2fa32523dee0f81f2fe4f3b6
Jinle Chen <cjinle@gmail.com>
2014-01-29 10:00:33 +0800

    from git_push.sh

--- flask_proj/hello.py
+++ flask_proj/hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, abort, redirect, url_for
+from flask.templating import render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,8 +10,10 @@ def index():
 
 @app.route("/hello")
 def hello():
-    return "Hello World!"
-    
+    s = 'world'
+    l = range(100, 120)
+    return render_template('hello2.html', name=s, nums=l)
+
 @app.route("/l")
 def l():
     return redirect(url_for('index'))
@@ -22,4 +25,4 @@ def show_user_profile(username):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
