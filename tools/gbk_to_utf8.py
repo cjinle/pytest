db380303068e0010d5516e3caa9f43753b24232c
Jinle Chen <cjinle@gmail.com>
2023-04-30 01:56:52 +0800

    iconv

--- tools/gbk_to_utf8.py
+++ tools/gbk_to_utf8.py
@@ -1,5 +1,53 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import subprocess, time
 
-print("hello world")
+
+# iconv -f gbk -t utf8 ConfigManager.cpp -o ConfigManager.cpp
+def iconv(filename):
+    print("start iconv", filename)
+    cmd = [
+        "iconv",
+        "-f",
+        "gbk",
+        "-t",
+        "utf8",
+        filename,
+        "-o",
+        filename,
+    ]
+    subprocess.call(cmd)
+
+
+
+# find . -name "*.py" -or -name "*.md"
+
+cmd = [
+    "find",
+    # "/data/wwwroot/server/server_hallid",
+    "/data/wwwroot/server/hallid",
+    "-name",
+    "*.cpp",
+    "-or",
+    "-name",
+    "*.h",
+    "-or",
+    "-name",
+    "*.ini"
+]
+
+result = subprocess.check_output(cmd)
+# print(type(result))
+filelist = result.decode("utf-8").split("\n")
+for filename in filelist:
+    if not filename: continue
+    # print(filename)
+    iconv(filename)
+
+# filename = "/data/wwwroot/server/hallid/Frame/Alloc/src/ConfigManager.h"
+# iconv(filename)
+
+
+
+
